0654e9a29aa057bcfc647a1a545ea072fde38217
Eros-Erebo <147782490+Eros-Erebo@users.noreply.github.com>
2024-10-06 23:17:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const membros = JSON.parse(localStorage.getItem('membros')) || [
-        { nome: 'Galliard', xp: 971330, nivel: 57 },
-        { nome: 'Willian Vaude', xp: 863310, nivel: 54 },
-        { nome: 'Darker', xp: 544860, nivel: 43 },
-        { nome: 'Feye Vaude', xp: 571550, nivel: 44 },
-        { nome: 'Eöl', xp: 282470, nivel: 31 },
-        { nome: 'Aysha Velarion', xp: 241100, nivel: 29 },
-        { nome: 'Legolas', xp: 151200, nivel: 23 },
-        { nome: 'Kota', xp: 179680, nivel: 25 },
-        { nome: 'Zoro', xp: 18000, nivel: 8 },
+        { nome: 'Galliard', xp: 1244530, nivel: 65 },
+        { nome: 'Willian Vaude', xp: 1367324, nivel: 68 },
+        { nome: 'Darker', xp: 895420, nivel: 55 },
+        { nome: 'Feye Vaude', xp: 732100, nivel: 50 },
+        { nome: 'Eöl', xp: 781710, nivel: 52 },
+        { nome: 'Aysha Velarion', xp: 677900, nivel: 48 },
+        { nome: 'Kota', xp: 290580, nivel: 32 },
+        { nome: 'Zoro', xp: 68500, nivel: 16 },
         { nome: 'Elenion', xp: 4800, nivel: 5 },
         { nome: 'Gauriel', xp: 0, nivel: 1 },
         { nome: 'Lua', xp: 0, nivel: 1 },
-        { nome: 'Alessa', xp: 0, nivel: 1 },
-        { nome: 'Kryzne', xp: 7500, nivel: 6 },
+        { nome: 'Alessa', xp: 3000, nivel: 4 },
+        { nome: 'Kryzne', xp: 21700, nivel: 9 },
     ];
 
     const inputTableBody = document.getElementById('inputTableBody');
