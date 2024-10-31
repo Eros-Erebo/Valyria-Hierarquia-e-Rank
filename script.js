053dc58338ae62f665e54d71f5e2c9676392f927
Eros-Erebo <147782490+Eros-Erebo@users.noreply.github.com>
2024-10-31 05:03:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { nome: 'Elenion', xp: 4800, nivel: 5 },
         { nome: 'Lua', xp: 0, nivel: 1 },
         { nome: 'Alessa', xp: 3000, nivel: 4 },
+        { nome: 'Yuno', xp: 0, nivel: 1 },
     ];
 
     const inputTableBody = document.getElementById('inputTableBody');
